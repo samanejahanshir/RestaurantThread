0478a42aeb2c733719f50f95779bda8eb2c34a78
unknown <jahanshir.sm92@gmail.com>
2021-11-13 21:57:21 -0800

    add property in class Customer

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,4 +1,34 @@
 package models;
 
+import models.enums.CustomerState;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer  {
+    private  String name;
+    private CustomerState state;
+    private  int orderNum;
+    private List<Order> orderList=new ArrayList<>();
+
+    public Customer(String name, List<Order> orderList) {
+        this.name = name;
+        this.orderList = orderList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public CustomerState getState() {
+        return state;
+    }
+
+    public void setState(CustomerState state) {
+        this.state = state;
+    }
 }
