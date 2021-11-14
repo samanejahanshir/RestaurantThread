1efb592ccac24f789d57d7172113e70cafcad3da
unknown <jahanshir.sm92@gmail.com>
2021-11-13 21:58:57 -0800

    add property in class Restaurant

--- src/service/Restaurant.java
+++ src/service/Restaurant.java
@@ -1,4 +1,21 @@
 package service;
 
+import models.Cook;
+import models.Customer;
+import models.Machine;
+import models.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
+    public  static int customerCount;
+    public  List<Cook> cookList=new ArrayList<>();
+    public  List<Customer> customerList=new ArrayList<>();
+    public  List<Machine> machineList=new ArrayList<>();
+    public  List<Order> orderList=new ArrayList<>();
+
+
+
+
 }
