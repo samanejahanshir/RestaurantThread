61c73a3bbcc23dfc42564ec62f7610de1ea9b6a6
unknown <jahanshir.sm92@gmail.com>
2021-11-13 21:34:09 -0800

    add CustomerState enum

--- src/models/enums/CustomerState.java
+++ src/models/enums/CustomerState.java
@@ -1,4 +1,18 @@
 package models.enums;
 
 public enum CustomerState {
+    CUSTOMER_STARTING("CustomerStarting"),
+    CUSTOMER_ENTERED("CustomerEntered"),
+    CUSTOMER_PLACE_ORDER("CustomerPlaceOrder"),
+    CUSTOMER_RECEIVE_ORDER("CustomerReceiveOrder"),
+    CUSTOMER_LEAVING("CustomerLeaving");
+    private  String state;
+
+    CustomerState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
