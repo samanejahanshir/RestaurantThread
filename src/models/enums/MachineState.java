7e65ab1e9e34f72b619b358db52975395ca90fb5
unknown <jahanshir.sm92@gmail.com>
2021-11-13 21:34:38 -0800

    add MachineState enum

--- src/models/enums/MachineState.java
+++ src/models/enums/MachineState.java
@@ -1,4 +1,16 @@
 package models.enums;
 
 public enum MachineState {
+    MACHINE_STARTING("MachineStarting"),
+    MACHINE_STARTING_FOOD("MachineStartingFood"),
+    MACHINE_DONE_FOOD("MachineDoneFood");
+    private  String state;
+
+    MachineState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
