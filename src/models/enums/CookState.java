51140427741e1d11f5e8b6daa3364b44184f239b
unknown <jahanshir.sm92@gmail.com>
2021-11-13 21:33:33 -0800

    add CookState enum

--- src/models/enums/CookState.java
+++ src/models/enums/CookState.java
@@ -1,4 +1,19 @@
 package models.enums;
 
 public enum CookState {
+    COOK_STARTING("CookStarting"),
+    COOK_RECEIVE_ORDER("CookReceiveOrder"),
+    COOK_START_FOOD("CookStartFood"),
+    COOK_FINISHED_FOOD("CookFinishedFood"),
+    COOK_COMPLETE_ORDER("CookCompleteOrder"),
+    COOK_ENDING("CookEnding");
+    private  String state;
+
+    CookState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
