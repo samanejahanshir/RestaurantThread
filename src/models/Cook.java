e41128f2779064c0f83e657668da22ddfb36ac2a
unknown <jahanshir.sm92@gmail.com>
2021-11-13 21:56:48 -0800

    add proerty in class Cook

--- src/models/Cook.java
+++ src/models/Cook.java
@@ -1,4 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cook {
+    private String name;
+    private List<Order> orderList=new ArrayList<>();
+    private  List<Machine> machineList=new ArrayList<>();
+
+    public Cook(String name, List<Order> orderList, List<Machine> machineList) {
+        this.name = name;
+        this.orderList = orderList;
+        this.machineList = machineList;
+    }
 }
