8a41b12be2f2adc8df263ee61620cb9ce847ea37
unknown <jahanshir.sm92@gmail.com>
2021-11-17 09:32:37 -0800

    add main in class Main

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,68 @@
 package view;
 
+import models.Cook;
+import models.Customer;
+import models.Machine;
+import models.enums.MachineFoodType;
 import service.Restaurant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        Restaurant restaurant=new Restaurant();
+    public static void main(String[] args) throws InterruptedException {
+        List<Cook> cookList = new ArrayList<>();
+        List<Customer> customerList = new ArrayList<>();
+        List<Machine> machineList = new ArrayList<>();
+        Restaurant restaurant = new Restaurant();
+        Machine cookPizza = new Machine("cookPizza", MachineFoodType.PIZZA.getFood(), 3, restaurant);
+        cookPizza.setName("cookPizza");
+        Machine cookSandwich = new Machine("CookSandwich", MachineFoodType.SANDWICH.getFood(), 2, restaurant);
+        cookSandwich.setName("CookSandwich");
+        Machine cookRice = new Machine("CookRice", MachineFoodType.RICE.getFood(), 2, restaurant);
+        cookRice.setName("CookRice");
+        Machine cookStake = new Machine("CookStake", MachineFoodType.STAKE.getFood(), 3, restaurant);
+        cookStake.setName("CookStake");
+        machineList = List.of(cookPizza, cookSandwich, cookRice, cookStake);
+
+        Cook sahar = new Cook("sahar", restaurant);
+        sahar.setName("Cook sahar");
+        Cook ali = new Cook("ali", restaurant);
+        ali.setName("Cook ali");
+        Cook hadi = new Cook("hadi", restaurant);
+        hadi.setName("Cook hadi");
+        Cook zahra = new Cook("zahra", restaurant);
+        zahra.setName("Cook zahra");
+        cookList = List.of(sahar, ali, hadi, zahra);
+        Customer sara = new Customer("sara",restaurant);
+        sara.setName("Customer sara");
+        Customer mahdi = new Customer("mahdi",restaurant);
+        mahdi.setName("Customer mahdi");
+        Customer fateme = new Customer("fateme",restaurant);
+        fateme.setName("Customer fateme");
+        customerList = List.of(sara, mahdi, fateme);
+        // Customer samane = new Customer(restaurant);
+        //  Customer farhad = new Customer(restaurant);
+        //  Customer reza = new Customer(restaurant);
+        restaurant.customerList=customerList;
+        restaurant.cookList=cookList;
+        restaurant.machineList=machineList;
+        List<Thread> threads = new ArrayList<>();
+        threads.addAll(machineList);
+        threads.addAll(cookList);
+        threads.addAll(customerList);
+        threads.forEach(Thread::start);
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+
+
+
     }
 }
